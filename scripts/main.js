2e302bbb97fb8b1b20706b1d31468d70a100e856
Tomasz Gacek <tomasz.gacek@mf.gov.pl>
2019-12-17 10:18:40 +0100

    lotto

--- scripts/main.js
+++ scripts/main.js
@@ -1,34 +1,36 @@
 
-function getRandomNumber(from,to) {
-    return Math.round( Math.random()+(to-from)+from);
+function getRandomNumber(from = 1, to = 49) {
+    return Math.round(Math.random() + (to - from) + from);
 };
 
-function buildArrayWithRandomNumbers(quantity = 6) 
-    {
-        return Array(quantity).fill(0).map(function() {
-            return getRandomNumber();
-        });
-    };
-    function render(list) {
+function buildArrayWithRandomNumbers(quantity = 6) {
+    return Array(quantity).fill(0).map(function () {
+        return getRandomNumber();
+    });
+}
+function render(list) {
+    let html = '';
+    function template(item) {
+        return `<span>${item}</span`;
 
-        let html ='';
-        function template(item) {
-        return '<span>${value}</span';
+    }
 
-        }
+    list.forEach(function (item) {
+        html += template(item);
+    });
 
-    list.forEach(function(item) {
-        html +=template(item);
-});
+    console.log(html);
+    const $page = document.querySelector('#page');
+    $page.innerHTML = html;
+}
 
-    };
 function main() {
-    const numbers=buildArrayWithRandomNumbers();
+    const numbers = buildArrayWithRandomNumbers();
     render(numbers);
-};
+}
 
 
-window.addEventListener('DOMContentLoaded',main)
+window.addEventListener('DOMContentLoaded', main)
 
 
 
